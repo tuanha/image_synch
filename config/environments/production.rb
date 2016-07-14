d3bb16c0cf0805f6082408aca4cb4b79f1d50ecc
tuanha <tuanha@hiworld.com.vn>
2016-07-14 16:02:00 +0700

    config send mail use gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,9 +75,17 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'imagesync.hiworld.com.vn' }
+  # these options are only needed if you choose smtp delivery
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'imagesync.hiworld.com.vn',
+    :authentication => :plain,
+    :user_name      => 'imagesynch@gmail.com',
+    :password       => 'kehuydiet',
+    :enable_starttls_auto => true
+  }
+
 end
